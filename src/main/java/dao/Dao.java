fb72cce2def2fdf060a63a001d6daa3b30182035
luanadquynh <134347518+luanadquynh@users.noreply.github.com>
2023-05-23 14:43:10 +0700

    Update Dao.java

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -4,11 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-/**
- * createAt Dec 24, 2020
- *
- * @author Đỗ Tuấn Anh <daclip26@gmail.com>
- */
+
 public abstract class Dao<T> {
 
     Connection conn = Database.getInstance().getConnection();
